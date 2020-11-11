5f6b5e9544446e7b6c7e929e8a1f57f81da3e5d9
Mathieu Harvey <mathieuharvey@Mathieus-MacBook-Pro.local>
2020-11-11 15:36:49 -0800

    fixed props on nav bar + tutor name

--- tutorPool/comps/TutorName/index.js
+++ tutorPool/comps/TutorName/index.js
@@ -2,31 +2,49 @@ import React from 'react';
 import {View, Text, Image, StyleSheet} from 'react-native';
 import propTypes from 'prop-types';
 
-const TutorName = () => {
+const TutorName = ({tutorname, tutorsubject, rating, imgsrc}) => {
     return <View style={styles.tutorNameContainer}>
-        
+        <View>
+            <Image source={imgsrc}></Image>
+        </View>
+        <View>
+            <Text style={styles.tutorNameText}>{tutorname}</Text>
+            <Text style={styles.tutorSubjectText}>{tutorsubject}</Text>
+            <Text style={styles.tutorRateText}>{rating}</Text>
+        </View>
     </View>
 }
 
 const styles=StyleSheet.create({
     tutorNameContainer: {
-        backgroundColor:"#F4F5F5",
-        width: 360,
-        height: 60,
-        display:"flex",
-        justifyContent:"space-between",
-        alignItems:"center",
-        flexDirection: "row",
-        padding: 30
-    }
+        
+    },
+
+    tutorNameText: {
+        color: "black",
+    },
+
+    tutorSubjectText: {
+        
+    },
+
+    tutorRateText: {
+        
+    },
+
 });
 
 TutorName.defaultprops = {
-    text:"Default",
+    tutorname:"Becca Williams",
+    tutorsubject:"English",
+    rating:"4.5",
+    imgsrc: require('../../images/icon.png')
 }
 
 TutorName.propTypes = {
-    text:propTypes.string,
+    tutorname:propTypes.string,
+    tutorsubject:propTypes.string,
+    rating:propTypes.string,
 }
 
 export default TutorName;

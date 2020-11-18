02f20a38316e2dbab3ab47edd2212be3ded847fb
kaye delos reyes <kayedelosreyes@kayes-MacBook-Pro.local>
2020-11-17 16:29:40 -0800

    profile tutor page

--- tutorPool/pages/ProfileTutor/index.js
+++ tutorPool/pages/ProfileTutor/index.js
@@ -7,52 +7,87 @@ import AboutInput from '../../comps/AboutInput';
 import Rates from '../../comps/Rates';
 import AvailableinText from '../../comps/AvailableinText';
 import SmallBubbleButton from '../../comps/SmallBubbleButton';
+import TutorNameEdit from '../../comps/TutorNameEdit';
+import BlueButton from '../../comps/BlueButton';
 
 
 
 const ProfileTutor = ({}) => {
     return <View style={styles.viewCont}>
         <TopNav />
-        <View style={styles.navbarBox}>
-        <Text>Profile</Text>
-        <UserCard style={styles.User} />
+        <Text style={styles.Profile}>Profile</Text>
+        <View style={styles.Box}>
+        <TutorNameEdit style={styles.Tutor} tutorname="Becca Williams" tutorsubject1="English," tutorsubject2="Art"/>
         <AboutInput name="About Me"/>
         <Rates />
         <View style={styles.Avail}>
-        <AvailableinText text="Available in"/>
+        <View style={styles.AvailText}><AvailableinText text="Available in"/></View>
         <SmallBubbleButton text="Video Calls"/>
         <SmallBubbleButton text="In-Person"/>
         </View>
-        <NavBar />
+        <View style={styles.UpdateButton}><BlueButton text="Update"/></View>
         </View>
+        <View style={styles.NavBar}><NavBar /></View>
     </View>
 }
 
 const styles=StyleSheet.create({
+
     viewCont: {
         display:"flex",
-        alignItems:"center",
-        justifyContent:"center",
+        
         backgroundColor:'white',
         marginTop:40,
+        margin:30,
 
     },
 
-    navbarBox: {
+    Profile: {
+        fontSize:25,
+        fontWeight: "500",
+        marginTop:43,
+    },
+
+    Tutor: {
+        marginTop: 30,
+    },
+
+    Box: {
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"space-around",
+        marginTop: 30,
+        backgroundColor:"#FFF",
+        borderColor:"#000",
+        borderWidth:1,
+
        
-    },
-
-    User: {
-        margin:50,
     },
 
     Avail: {
         width: 300,
-        display:"flex",
         flexDirection:"row",
         alignItems:"center",
-        justifyContent:"space-between",
-    }
+        justifyContent: "center",
+        justifyContent:"space-evenly",
+        margin: 30,
+    },
+
+    AvailText: {
+        marginBottom:15,
+        marginRight: 30,
+        
+    },
+
+    UpdateButton: {
+        marginBottom:10
+
+    },
+
+    NavBar: {
+        
+    },
+
 
 });
 
